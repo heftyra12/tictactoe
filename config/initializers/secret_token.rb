7b7a2ac5d4ba4a727b09a40a5abb40ea3b5c2282
heftyra12 <heftyra12@uww.edu>
2011-12-05 21:44:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tictactoe::Application.config.secret_token = '591cf0f2315ed579b28c029f9b7091fc5f15982a79c5244b78721b72bad219ec3f60d6f82c27c66f586de97f9bfb3b5af90ec053a6192af77dac3f3e14d7fd48'
